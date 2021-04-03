2006df2df31342d4e24a8a7469a3b3d15bfe9f26
Ahmed Elhanafy <ahmed.elhanafy95@gmail.com>
2021-04-04 00:51:03 +0200

    Initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,6 @@
+export const Footer = () => (
+  <footer>
+    <hr />
+    <span>I'm here to stay (Footer)</span>
+  </footer>
+);
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout title="Ahmed Elhanafy">
+    <h1>Ahmed Elhanafy</h1>
+  </Layout>
+);
+
+export default IndexPage;
